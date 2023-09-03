280bea232dc437894c632af3864c0a743d6bdcc7
roberthopman <hopman.r@gmail.com>
2023-09-03 18:26:52 +0200

    Remove alert

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Remove the flash message after 5 seconds
+document.addEventListener('turbo:load', function() {
+  setTimeout(function() {
+  var alertElement = document.querySelector('[role="alert"]');
+  if (alertElement) {
+    alertElement.remove();
+  }}, 1500);
+});
